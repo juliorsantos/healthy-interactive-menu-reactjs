7f07bc7c09f9c3f548019db597f0cb22e09fea96
Julio <julioccpv@gmail.com>
2022-05-20 17:09:16 -0300

    Creating a component for Item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,25 @@
 const Item = ({ data }) => {
 
+  function moneyFormatter(price) {
+    // After we will move and change it.
+    return price;
+  }
+
   return (
-    <div className="item">
-      <h1>{data.name}</h1>
+    <div className="product-item">
+      <div className="image-wrapper">
+        <img src={data.image} />
+      </div>
+      <h3 className='title'>{data.title}</h3>
+      <h5 className='price'>{moneyFormatter(data.price)}</h5>
+      <div className="btns d-flex flex-fill">
+        <button className='flex-fill m-1 btn btn-secondary' type='button'>
+          ℹ Details
+        </button>
+        <button className='flex-fill m-1 btn btn-success' type='button'>
+          🛒 Order
+        </button>
+      </div>
     </div>
   )
 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,9 @@
 import Banner from '../components/Banner';
 import ProductList from './../../public/foods.json';
 
-const Index = () => {
+import Item from './../components/Item';
 
-  function moneyFormatter(price) {
-    // After we will move and change it.
-    return price;
-  }
+const Index = () => {
 
   return (
     <>
@@ -14,23 +11,9 @@ const Index = () => {
       <div className="container py-2">
         <h1 className='text-center py-4'>Order our delicious food</h1>
         <div className="row products-row">
-          {ProductList && ProductList.map((item, key) => (
+          {ProductList && ProductList.map((data, key) => (
             <div className='col-lg-3 col-sm-12' key={key}>
-              <div className="product-item">
-                <div className="image-wrapper">
-                  <img src={item.image} />
-                </div>
-                <h3 className='title'>{item.title}</h3>
-                <h5 className='price'>{moneyFormatter(item.price)}</h5>
-                <div className="btns d-flex flex-fill">
-                  <button className='flex-fill m-1 btn btn-secondary' type='button'>
-                    ℹ Details
-                  </button>
-                  <button className='flex-fill m-1 btn btn-success' type='button'>
-                    🛒 Order
-                  </button>
-                </div>
-              </div>
+              <Item data={data} />
             </div>
           ))}
         </div>

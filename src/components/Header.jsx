29bfd8f36992e9a308a72c9baa74e260d8fae9b5
Julio <julioccpv@gmail.com>
2022-05-20 17:25:35 -0300

    Adjusting the header background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
 
   return (
-    <nav className="navbar navbar-expand-lg">
+    <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
         <div className="navbar-brand">
           <h5>GoodFood</h5>
